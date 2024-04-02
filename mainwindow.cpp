26bc22f64c9da0878e1f5f1df5de3dc13af334c4
TymofiiPys <nfstimoha2004@gmail.com>
2024-04-02 18:00:13 +0300

    Add opening and displaying an image

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_actionExit_triggered() { QApplication::exit(); }
+
+void MainWindow::on_actionOpenFile_triggered() {
+  QString imagePath = QFileDialog::getOpenFileName(
+      this, QString("Відкрити файл..."), QDir::homePath(),
+      QString("Зображення (*.jpg *.jpeg *.png *.bmp *.gif)"));
+
+  this->openedImage = QImage(imagePath);
+
+  QGraphicsScene* imageViewScene = new QGraphicsScene(this);
+  imageViewScene->addPixmap(QPixmap::fromImage(this->openedImage));
+  imageViewScene->setSceneRect(this->openedImage.rect());
+  ui->imageView->setScene(imageViewScene);
+  ui->imageView->scale(0.25, 0.25);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -9,15 +9,29 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+private slots:
+  /*!
+   * \brief Exit program and prompt for saving
+   * \internal
+   */
+  void on_actionExit_triggered();
+
+  /*!
+   * \brief Open file for processing, prompt for saving already opened one
+   * \internal
+   */
+  void on_actionOpenFile_triggered();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+
+    QImage openedImage;
 };
 #endif // MAINWINDOW_H
